b0ed04ffd20c544733927c2e734018328facb1cb
Oren Me <oren.me@gmail.com>
2015-12-28 16:23:41 +0200

    Candidate

--- js/data.js
+++ js/data.js
@@ -1,47 +1,44 @@
-/**
- * Created by itayk on 27/12/15.
- */
-var stream1 = {
-	src:"http://lbd.kaltura.com:8001/mapped/hackathon.php/file/1/manifest.mpd",
+var stream1_ = {
+	src:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/0/manifest.mpd",
 	channel:1,
 	meta:"kaka",
 	x:0,
 	y:0,
-	width:480,
-	height:270
+	width:320,
+	height:180
 };
-var stream2 = {
-	src:"http://lbd.kaltura.com:8001/mapped/hackathon.php/file/2/manifest.mpd",
+var stream2_ = {
+	src:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/1/manifest.mpd",
 	channel:2,
 	meta:"kaka",
-	x:480,
+	x:320,
 	y:0,
-	width:480,
-	height:270
+	width:320,
+	height:180
 };
-var stream3 = {
-	src:"http://lbd.kaltura.com:8001/mapped/hackathon.php/file/3/manifest.mpd",
+var stream3_ = {
+	src:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/2/manifest.mpd",
 	channel:3,
+	meta:"kaka",
+	x:640,
+	y:0,
+	width:320,
+	height:180
+};
+var stream4_ = {
+	src:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/3/manifest.mpd",
+	channel:4,
 	meta:"kaka",
 	x:960,
 	y:0,
-	width:480,
-	height:270
-};
-var stream4 = {
-	src:"http://lbd.kaltura.com:8001/mapped/hackathon.php/file/4/manifest.mpd",
-	channel:4,
-	meta:"kaka",
-	x:1440,
-	y:0,
-	width:480,
-	height:270
+	width:320,
+	height:180
 };
 var data = {
-	streams:[stream1,stream2,stream3,stream4],
-	mainSrc:"http://lbd.kaltura.com:8001/mapped/hackathon.php/file/strip/manifest.mpd",
+	streams:[stream1_,stream2_,stream3_,stream4_],
+	mainSrc:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/strip/manifest.mpd",
 	data:"myData",
-	width:480,
-	height:270
+	width:320,
+	height:180
 
 };
--- js/multiple-video-handler.js
+++ js/multiple-video-handler.js
@@ -1,9 +1,11 @@
 /**
  * Created by itayk on 27/12/15.
  */
+
+
 var multipleVideoHandler = function(strip,main,canvas,data,shaka){
 	var _this = this;
-	this.video = strip;
+	this.stripVideo = strip;
 	this.mainVideo = main;
 	this.canvas = canvas;
 	this.data = data;
@@ -15,72 +17,211 @@ var multipleVideoHandler = function(strip,main,canvas,data,shaka){
 	this.canvas.height = this.data.height;
 
 	shaka.polyfill.installAll();
-	this.player = new shaka.player.Player(this.video);
+	this.stripPlayer = new shaka.player.Player(this.stripVideo);
+	this.stripPlayer.configure({
+		streamBufferSize: 40
+	});
 	this.mainPlayer = new shaka.player.Player(this.mainVideo);
+	this.mainPlayer.configure({
+		streamBufferSize: 10
+	});
+	var mainEstimator = new shaka.util.EWMABandwidthEstimator();
+	var mainSource = new shaka.player.DashVideoSource(this.currentStream.src, null, mainEstimator);
+	var stripEstimator = new shaka.util.EWMABandwidthEstimator();
+	var stripSource = new shaka.player.DashVideoSource(this.mainSrc, null, stripEstimator);
 
-	var estimator = new shaka.util.EWMABandwidthEstimator();
-	var source = new shaka.player.DashVideoSource(this.mainSrc, null, estimator);
+	showVideo();
+	_this.mainPlayer.load(mainSource).then(function(){
+		_this.mainVideo.play();
+	});
 
-	_this.player.load(source).then(function(){
+	_this.stripPlayer.load(stripSource).then(function(){
 		context = _this.canvas.getContext("2d");
-		_this.audioTracks = _this.player.getAudioTracks();
-		_this.video.play();
+		_this.audioTracks = _this.stripPlayer.getAudioTracks();
+		_this.stripVideo.volume = 0;
+		_this.stripVideo.play();
 		paintFrame();
 	});
 
-	 _this.player.addEventListener('seekrangechanged', onSeekrangechanged);
-	_this.mainPlayer.addEventListener('seekrangechanged', onSeekrangechanged2);
-	function onSeekrangechanged(event) {
-		console.info( "onSeekrangechanged: start=" + event.start + ", end=" + event.end );
-
-	}
-	function onSeekrangechanged2(event) {
-		console.info( "onSeekrangechanged22: start=" + event.start + ", end=" + event.end );
-
+	_this.stripPlayer.addEventListener('trackschanged',onTrackChange);
+	function onTrackChange(event){
+		_this.stripVideo.volume = 1;
 	}
 
+	// _this.player.addEventListener('seekrangechanged', onSeekrangechanged);
+	//_this.mainPlayer.addEventListener('seekrangechanged', onSeekrangechanged2);
+	//function onSeekrangechanged(event) {
+	//	console.info( "onSeekrangechanged: start=" + event.start + ", end=" + event.end );
+    //
+	//}
+	//function onSeekrangechanged2(event) {
+	//	console.info( "onSeekrangechanged22: start=" + event.start + ", end=" + event.end );
+    //
+	//}
+	//setInterval(function(){
+	//	console.warn("mainVideo: " + (_this.mainVideo.currentTime) + ", stripVideo: " + (_this.stripVideo.currentTime));
+	//	console.warn("delta: " + (_this.mainVideo.currentTime-_this.stripVideo.currentTime));
+	//}, 500);
 
 
 	function updateSource(){
 		_this.mainVideo.pause();
 		_this.showCanvas();
 		_this.currentStream = _this.data.streams[_this.currentStreamIndex];
-		if (this.audioTracks){
-
+		if (_this.audioTracks){
+			_this.stripPlayer.selectAudioTrack(_this.currentStream.channel, true, 2);
+			_this.mainVideo.volume = 0;
+			_this.stripVideo.volume = 0;
+			setTimeout(function(){
+				_this.stripVideo.volume = 1;
+			}, 4000);
 		}
-		_this.mainPlayer.load(new shaka.player.DashVideoSource(_this.currentStream.src, null, estimator)).then(function(){
-			_this.mainVideo.addEventListener("playing",function(){
-				_this.showVideo();
-			});
-			_this.mainPlayer.setPlaybackStartTime(_this.video.currentTime/1000);
+		var playerCurrentStream = new shaka.player.DashVideoSource(_this.currentStream.src, null, mainEstimator);
+		_this.mainPlayer.load(playerCurrentStream).then(function(){
+			console.info("Main player loaded, register playing event");
+			var alignPlayers = function(){
+				console.info("Remove playing event handler");
+				_this.mainVideo.removeEventListener("playing", alignPlayers);
+				console.info("Main video is playing, try to sync...");
+				var boundedSync = sync.bind(_this);
+				boundedSync()
+						.then(function () {
+							_this.showVideo();
+							_this.mainVideo.volume = 1;
+							_this.stripVideo.volume = 0;
+						})
+						.catch(function () {
+							console.info("Failed sync, reset playback rates");
+						});
+			}.bind(_this);
+			_this.mainVideo.addEventListener("playing", alignPlayers);
+			_this.mainPlayer.setPlaybackStartTime(_this.stripVideo.currentTime + 10);
 			_this.mainVideo.play();
-
-
 		});
-
 	}
 
+	function sync(){
+		var count = 0;
+		var syncVideoInterval;
+		var boundedSyncVideo = syncVideo.bind(this);
+		var prom = new Promise(
+			function(resolve, reject) {
+				if (boundedSyncVideo()){
+					return resolve();
+				} else {
+					syncVideoInterval = setInterval(function() {
+						console.info("===============");
+						console.info("Checking if videos are synced ("+count+")");
+						if (boundedSyncVideo()){
+							clearInterval(syncVideoInterval);
+							console.info("Videos are synced");
+							return resolve();
+						}
+						if (count >= 50){
+							console.info("Videos are not synced after " + count + " retries");
+							clearInterval(syncVideoInterval);
+							return reject();
+						}
+						count = count + 1;
+					}, 1000);
+				}
+			});
+		return prom;
+	}
 
+	function syncVideo() {
+		var sct = ( this.stripVideo.currentTime );
+		var mct = ( this.mainVideo.currentTime );
+		var delta = Math.abs(sct - mct);
+		var adaptivePlaybackrateChange = Math.round(delta * 100) / 100;
+		console.info("Time difference = " + delta);
+		console.info("Main = " + mct + ", strip = " + sct);
+		if (delta < 0.1) {
+			//If delta is smaller than X then set playbackRate back to 1
+			this.mainVideo.playbackRate = 1;
+			this.seeking = false;
+			console.info("Delta less then 0.05, Go power rangers, Go!");
+			return true;
+		} else if(!this.mainVideo.seeking && !this.seeking){
+			//If delta is bigger than X then set playbackRate to Y to allow
+			//The videos to catch up
+			if (mct > sct) {
+				if (adaptivePlaybackrateChange >= 1){
+					this.mainVideo.pause();
+					console.info("Main video is ahead of strip, pausing playback");
+				} else {
+					this.mainVideo.play();
+					this.mainVideo.playbackRate = (1 - adaptivePlaybackrateChange);
+					console.info("Main video is ahead of strip, slowing playback rate down to " + this.mainVideo.playbackRate);
+				}
+
+			} else {
+				this.mainVideo.play();
+				this.mainVideo.playbackRate = 1;
+				this.seektarget = this.stripVideo.buffered.end(this.stripVideo.buffered.length-1);
+				console.info("Main video is behind of strip, seeking to " + this.seektarget);
+				this.mainVideo.currentTime = this.seektarget;
+				this.seeking = true;
+			}
+
+			return false;
+		} else {
+			if ((this.mainVideo.currentTime >= this.seektarget) ||
+				(this.mainVideo.currentTime >= this.stripVideo.currentTime) ||
+				(this.stripVideo.currentTime > this.seektarget)) {
+				this.seeking = false;
+			}
+			console.info("Waiting for seek to sync videos");
+			return false;
+		}
+	}
 
 	function paintFrame(){
-		context.drawImage(_this.video,_this.currentStream.x,_this.currentStream.y,_this.currentStream.width,_this.currentStream.height,0,0,_this.data.width,_this.data.height);
+		context.drawImage(_this.stripVideo,_this.currentStream.x,_this.currentStream.y,_this.currentStream.width,_this.currentStream.height,0,0,_this.data.width,_this.data.height);
 		requestAnimationFrame(paintFrame);
+	}
+
+	function showVideo(){
+		addClass(_this.canvas, "hidden");
+		removeClass(_this.mainVideo, "hidden");
+	}
+
+	function showCanvas(){
+		removeClass(_this.canvas, "hidden");
+		addClass(_this.mainVideo, "hidden");
+	}
+
+	function addClass(el, className){
+		if (el.classList)
+			el.classList.add(className);
+		else
+			el.className += ' ' + className;
+	}
+
+	function removeClass(el, className){
+		if (el.classList)
+			el.classList.remove(className);
+		else
+			el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 	}
 
 	//public API
 	this.showVideo = function(){
-	//	_this.canvas.style.visibility = "hidden";
-		 _this.mainVideo.style.visibility = "";
+		addClass(_this.canvas, "hidden");
+		removeClass(_this.mainVideo, "hidden");
 	};
 
 	this.showCanvas = function(){
-		_this.canvas.style.visibility = "";
-	//	_this.mainVideo.style.visibility = "hidden";
+		removeClass(_this.canvas, "hidden");
+		addClass(_this.mainVideo, "hidden");
+		//_this.canvas.style.visibility = "";
+		//_this.mainVideo.style.visibility = "hidden";
 	};
-	this.zapUp = function(){
-		_this.currentStreamIndex = (_this.currentStreamIndex + 1) % _this.data.streams.length;
-		updateSource();
+	this.zapUp = function(index){
+		if (index != _this.currentStreamIndex) {
+			var channel = (index != null) && (index != undefined) ? index : _this.currentStreamIndex + 1;
+			_this.currentStreamIndex = channel % _this.data.streams.length;
+			updateSource();
+		}
 	};
-
-
 };
