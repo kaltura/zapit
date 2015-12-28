a28ce0eed11f0fb02fbbfb37dac107622b6e8dff
Oren Me <oren.me@gmail.com>
2015-12-28 18:04:05 +0200

    Update data

--- js/data.js
+++ js/data.js
@@ -34,8 +34,17 @@ var stream4_ = {
 	width:320,
 	height:180
 };
+var stream5_ = {
+	src:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/4/manifest.mpd",
+	channel:5,
+	meta:"kaka",
+	x:1280,
+	y:0,
+	width:320,
+	height:180
+};
 var data = {
-	streams:[stream1_,stream2_,stream3_,stream4_],
+	streams:[stream1_,stream2_,stream3_,stream4_, stream5_],
 	mainSrc:"http://lbd.kaltura.com:8001/mapped/merge_manifest.php/file/strip/manifest.mpd",
 	data:"myData",
 	width:320,
